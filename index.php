e0172c40a648717e51a6f6127e27391978ac7203
shukri <abdishukrimohamed105@gmail.com>
2020-02-20 17:25:31 +0300

    The first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>my first PHP Website</title>
+</head>
+<body>
+	<?php 
+	   echo"<p>hello world!</p>";
+	   ?>
+	   <a href="login.php">click here to login <br>
+	   	<a href="register.php">click here to register
+
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>my first PHP Websute</title>
+</head>
+<body>
+	<h2>Registration Page </h2>
+    <a href="index.php">click here to go back</a><br/><br/>
+    <form action="register.php"method="POST">
+    	enter Username:<input type="text"name="Username"required="required"/><br/>
+    	enter password:<input type="password"name="password"required="required"/><br/>
+    	<input type="submit"value="register"/>
+    </form>
+
+</body>
+</html>
